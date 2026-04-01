b16b53b6323bc9b74d59398bac80b152ae422773
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-04-01 19:01:37 +0200

    Update: .devcontainer/devcontainer.json, Dockerfile, Makefile, src/Migrator.php

--- src/Migrator.php
+++ src/Migrator.php
@@ -25,13 +25,13 @@ use TomasChochola\Pdo\QueryInterface;
  */
 readonly class Migrator implements MigratorInterface
 {
-    public readonly LockerInterface $locker;
+    private readonly LockerInterface $locker;
 
-    public readonly LoggerInterface $logger;
+    private readonly LoggerInterface $logger;
 
-    public readonly MigrationsInterface $migrations;
+    private readonly MigrationsInterface $migrations;
 
-    public readonly QueryInterface $query;
+    private readonly QueryInterface $query;
 
     public function __construct(QueryInterface $query, LoggerInterface $logger, MigrationsInterface $migrations, LockerInterface $locker)
     {
