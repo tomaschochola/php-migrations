45a656047eda0442dc6c241b5f86d23e3c27da66
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-04-03 16:41:57 +0200

    Update: Dockerfile, composer.json, src/MigrationsInterface.php, src/Migrator.php

--- src/Migrator.php
+++ src/Migrator.php
@@ -1,44 +1,21 @@
 <?php
 
-/**
- * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
- * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
- *
- * @license CC-BY-ND-4.0
- *
- * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
- * @see {@link https://github.com/tomaschochola} GitHub Profile
- * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
- */
-
 declare(strict_types=1);
 
 namespace TomasChochola\Migrations;
 
 use Override;
 use Psr\Log\LoggerInterface;
-use TomasChochola\Pdo\LockerInterface;
-use TomasChochola\Pdo\QueryInterface;
 
-/**
- * @no-named-arguments
- */
 readonly class Migrator implements MigratorInterface
 {
-    private readonly LockerInterface $locker;
-
     private readonly LoggerInterface $logger;
-
     private readonly MigrationsInterface $migrations;
 
-    private readonly QueryInterface $query;
-
-    public function __construct(QueryInterface $query, LoggerInterface $logger, MigrationsInterface $migrations, LockerInterface $locker)
+    public function __construct(LoggerInterface $logger, MigrationsInterface $migrations)
     {
-        $this->query = $query;
         $this->logger = $logger;
         $this->migrations = $migrations;
-        $this->locker = $locker;
     }
 
     /**
@@ -48,33 +25,27 @@ readonly class Migrator implements MigratorInterface
     public function migrate(iterable $migrations): void
     {
         $this->logger->info('migrator.start');
+        $this->migrations->start();
 
-        $lock = $this->locker->lock('migrations', 3_600);
+        foreach ($migrations as $migration) {
+            $selector = $migration->selector();
 
-        try {
-            $this->migrations->init();
+            if ($this->migrations->has($selector)) {
+                $this->logger->info('migration.skip', ['selector' => $selector]);
+            } else {
+                $this->logger->info('migration.start', ['selector' => $selector]);
 
-            foreach ($migrations as $migration) {
-                $selector = $migration->selector();
-
-                if ($this->migrations->has($selector)) {
-                    $this->logger->info('migration.skip', ['selector' => $selector]);
-                } else {
-                    $this->logger->info('migration.start', ['selector' => $selector]);
-
-                    foreach ($migration->migrate() as $sql) {
-                        $this->logger->notice('migration.sql', ['selector' => $selector, 'sql' => $sql]);
-                        $this->query->run($sql);
-                    }
-
-                    $this->migrations->mark($selector);
-                    $this->logger->info('migration.done', ['selector' => $selector]);
+                foreach ($migration->migrate() as $sql) {
+                    $this->logger->notice('migration.sql', ['selector' => $selector, 'sql' => $sql]);
+                    $this->migrations->execute($sql);
                 }
+
+                $this->migrations->mark($selector);
+                $this->logger->info('migration.done', ['selector' => $selector]);
             }
-        } finally {
-            $lock->unlock();
         }
 
+        $this->migrations->end();
         $this->logger->info('migrator.done');
     }
 }
