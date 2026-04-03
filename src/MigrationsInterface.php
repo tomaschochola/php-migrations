4216e5792ecfbf72348b509670380832fa982416
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-04-04 01:53:29 +0200

    Update: src/MigrationsInterface.php, src/Migrator.php

--- src/MigrationsInterface.php
+++ src/MigrationsInterface.php
@@ -1,16 +1,31 @@
 <?php
 
+/**
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
 declare(strict_types=1);
 
 namespace TomasChochola\Migrations;
 
 use NoDiscard;
+use Stringable;
 
+/**
+ * @no-named-arguments
+ */
 interface MigrationsInterface
 {
     public function end(): void;
 
-    public function execute(string $sql): void;
+    public function execute(string|Stringable $sql): void;
 
     #[NoDiscard]
     public function has(string $selector): bool;
