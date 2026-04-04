8a79e6422a4cd04f1229c98ed41ac7ea4baa05e2
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-04-04 02:15:05 +0200

    Update: src/MigrationsInterface.php

--- src/MigrationsInterface.php
+++ src/MigrationsInterface.php
@@ -25,7 +25,7 @@ interface MigrationsInterface
 {
     public function end(): void;
 
-    public function execute(string|Stringable $sql): void;
+    public function execute(Stringable|string $sql): void;
 
     #[NoDiscard]
     public function has(string $selector): bool;
